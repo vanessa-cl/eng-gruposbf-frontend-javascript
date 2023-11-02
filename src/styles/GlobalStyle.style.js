c9062923b2c2533257da4a0c0e22bdef889ca88d
Vanessa Lima <vanessalima951@gmail.com>
2023-11-02 16:05:29 -0300

    creating global style

---
+++ src/styles/GlobalStyle.style.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+  :root {
+    --nike-primary: #000000;
+    --nike-neutral-100: #FFFFFF;
+    --nike-neutral-200: #F7F7F7;
+    --nike-neutral-300: #D6D6D6;
+    --nike-neutral-500: #737373;
+    --nike-neutral-700: #1A1A1A;
+    --centauro-success-300: #006611;
+    --nike-paragraph-tiny: 12px;
+    --nike-label-medium: 14px;
+    --nike-paragraph-medium: 16px;
+    --nike-heading-2: 18px;
+    --nike-heading-1: 32px;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+    font-style: normal;
+  }
+
+`;
