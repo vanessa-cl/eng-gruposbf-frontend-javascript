6f7abe7aafda3637d8962ba543c6ceeb280ef073
Vanessa Lima <vanessalima951@gmail.com>
2023-12-05 23:23:01 -0300

    fix: nextjs version

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const packageJson = require("./package.json");
+
+console.log(`NEXTJS VERSION: ${packageJson.dependencies.next}`);
+
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    styledComponents: true
-  }
-}
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
